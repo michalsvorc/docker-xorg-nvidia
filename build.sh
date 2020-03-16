4038bd99350ebf572569a7f98c59f19cccddb482
Michal Svorc <michal@svorc.sk>
2020-03-16 17:48:46 +0100

    Add image name prefix

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ get_nvidia_driver_version() {
 }
 
 # Base variables
-image_name='xorg-nvidia'
+image_name='michalsvorc/xorg-nvidia'
 image_tag_suffix='multiarch'
 base_image='ubuntu'
 base_image_tag='18.04'
@@ -22,13 +22,11 @@ image_tag_build="${base_image}-${base_image_tag}-${nvidia_driver_version}-${imag
 # Print Nvidia driver version
 printf "Nvidia driver version: %s\n" ${nvidia_driver_version}
 
-# Build
+# Build and tag finished build
 docker build \
     --tag "${image_name}:${image_tag_build}" \
     --build-arg base_image=${base_image} \
     --build-arg base_image_tag=${base_image_tag} \
     --build-arg nvidia_driver_version=${nvidia_driver_version} \
-    .
-
-# Tag finished build
-docker tag "${image_name}:${image_tag_build}" "${image_name}:${image_tag}"
+    . \
+    && docker tag "${image_name}:${image_tag_build}" "${image_name}:${image_tag}"
